799f8723d1bbe80d7c009366261ab87d1eaa9075
luigiCarter <propersuccess12@gmail.com>
2025-02-25 16:50:41 -0500

    created form object

--- static/scripts.js
+++ static/scripts.js
@@ -1,4 +1,37 @@
 const coreEmployeeElement = document.getElementById('core-personel');
+const employee = document.getElementById('employ-status');
+const start_date = document.getElementById('start-date');
+
+const form_fields = {
+  startDate: undefined,
+  endDate: undefined,
+  firstName: undefined,
+  lastName: undefined,
+  phoneNumber: undefined,
+  email: undefined,
+  address: undefined,
+  employeeStatus: undefined,
+  typeOfEmployee: undefined,
+  fromPublicService: undefined,
+  SupervisorOrManager: undefined,
+  performancePay: undefined,
+  buildingLocation: undefined,
+  teleworkOrOnsite: undefined,
+  employeeTitle: undefined,
+  employeeSector: undefined,
+  employeeBranch: undefined,
+  employeeDirectorate: undefined,
+  SupervisorName: undefined,
+  numberOfMonitors: undefined,
+  KeyboardAndMouse: undefined,
+  otherEquipment: undefined,
+  cellphone: undefined,
+  cellSupervisorOrManager: undefined,
+  mailboxAccess: undefined,
+  software: undefined,
+  Gcdocs: undefined,
+  other: undefined,
+};
 
 function coreEm() {
   if (coreEmployeeElement === null) {
@@ -8,4 +41,18 @@ function coreEm() {
   }
 }
 
-coreEmployeeElement.addEventListener('click', coreEm);
+// function dropdown_selection(component) {
+//   if (component === ){
+
+//   }
+// }
+
+start_date.addEventListener("", () => {
+  if (start_date)
+});
+
+employee.addEventListener('click', (e) => {
+  console.log(e.value);
+});
+
+console.log();
