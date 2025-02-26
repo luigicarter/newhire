cf32e4572b7827bde0093b5c184031971acb53c5
luigiCarter <propersuccess12@gmail.com>
2025-02-26 12:28:22 -0500

    all variables for each field even conditionnal fields have been made

--- static/scripts.js
+++ static/scripts.js
@@ -1,8 +1,10 @@
+/// all static fields
 const startDate = document.getElementById('start-date');
 const endDate = document.getElementById('end-date');
 const firstName = document.getElementById('first-name');
 const lastName = document.getElementById('last-name');
 const phoneNumber = document.getElementById('phone');
+const email = document.getElementById('email');
 const address = document.getElementById('address');
 const employeeStatus = document.getElementById('employ-status');
 const buildingLocation = document.getElementById('building-location');
@@ -13,50 +15,35 @@ const employeeDirectorate = document.getElementById('directorate');
 const SupervisorName = document.getElementById('supervisor-name');
 const numberOfMonitors = document.getElementById('number-of-monitors');
 const KeyboardAndMouse = document.getElementById('keyboard-mouse');
-const otherEquipment = 5;
-const coreEmployeeElement = document.getElementById('core-personel');
-const employee = document.getElementById('employ-status');
+const otherEquipmentCheck = document.getElementById('other-equipment-check');
+const otherEquipmentText = document.getElementById('other-equipment-textbox');
+const mailboxAccess = document.getElementById('access-to-shared-mailbox');
+const softwareAccess = document.getElementById('software-access');
+const otherRequirments = document.getElementById('other-requirements');
 
-/// add all fields except for check box fields and dropdown
+//check box fields
+//////////////// type of employee
+const employeeCorePersonel = document.getElementById('core-personel');
+const employeeESSPersonel = document.getElementById('ESS-personel');
+////////// employee is from public check check boxes ?
+const FromPublicService = document.getElementById('from-public-service');
+const notFromPublicService = document.getElementById('not-from-public-service');
+//////// is user the supervisor
+const isSupervisor = document.getElementById('isSupervisor');
+const notSupervisor = document.getElementById('NotSupervisor');
+//////// performance pay check boxes
+const yesPerformancePay = document.getElementById('performance-pay-yes');
+const noPerformancePay = document.getElementById('performance-pay-no');
+////////// onsite work
+const telework = document.getElementById('telework');
+const onSite = document.getElementById('onsite');
+///////// cell phone
+const yesCell = document.getElementById('yes-cellphone');
+const noCell = document.getElementById('no-cellphone');
+const isManagerOrSupervisor = document.getElementById('isManagement-cell');
+////////// GCDOCS access
+const yesGCDOCS = document.getElementById('YesGCdocs');
+const noGCDOCS = document.getElementById('NoGCDOCS');
 
-/// write functions to assign values to the check box fields and dynamically grey them out when
-// one is selected.
-
-// const form_fields = {
-//   startDate: undefined,
-//   endDate: undefined,
-//   firstName: undefined,
-//   lastName: undefined,
-//   phoneNumber: undefined,
-//   email: undefined,
-//   address: undefined,
-//   employeeStatus: undefined,
-//   typeOfEmployee: undefined,
-//   fromPublicService: undefined,
-//   SupervisorOrManager: undefined,
-//   performancePay: undefined,
-//   buildingLocation: undefined,
-//   teleworkOrOnsite: undefined,
-//   employeeTitle: undefined,
-//   employeeSector: undefined,
-//   employeeBranch: undefined,
-//   employeeDirectorate: undefined,
-//   SupervisorName: undefined,
-//   numberOfMonitors: undefined,
-//   KeyboardAndMouse: undefined,
-//   otherEquipment: undefined,
-//   cellphone: undefined,
-//   cellSupervisorOrManager: undefined,
-//   mailboxAccess: undefined,
-//   software: undefined,
-//   Gcdocs: undefined,
-//   other: undefined,
-// };
-
-function coreEm() {
-  if (coreEmployeeElement === null) {
-    console.log('core EM check box is unchecked');
-  } else {
-    console.log(coreEmployeeElement.value);
-  }
-}
+////////////// write functions to assign values to the check box fields and dynamically grey them out when
+//////////one is selected.
