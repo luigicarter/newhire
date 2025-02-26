ef86d4b76736e7914176af819b48e3b4f006c386
luigiCarter <propersuccess12@gmail.com>
2025-02-26 08:53:02 -0500

    commit to save progress

--- static/scripts.js
+++ static/scripts.js
@@ -1,8 +1,20 @@
 const startDate = document.getElementById('start-date');
 const endDate = document.getElementById('end-date');
-
+const firstName = document.getElementById('first-name');
+const lastName = document.getElementById('last-name');
+const phoneNumber = document.getElementById('phone');
+const address = document.getElementById('address');
+const employeeStatus = document.getElementById('employ-status');
+const buildingLocation = document.getElementById('building-location');
+const employeeTitle = document.getElementById('employee-title');
+const employeeSector = document.getElementById('employee-sector');
+const employeeBranch = document.getElementById('employee branch');
+const employeeDirectorate = document.getElementById('directorate');
+const SupervisorName = document.getElementById('supervisor-name');
+const numberOfMonitors = document.getElementById('number-of-monitors');
+const KeyboardAndMouse = document.getElementById('keyboard-mouse');
+const otherEquipment = 5;
 const coreEmployeeElement = document.getElementById('core-personel');
-<<<<<<< HEAD
 const employee = document.getElementById('employ-status');
 
 /// add all fields except for check box fields and dropdown
@@ -10,38 +22,36 @@ const employee = document.getElementById('employ-status');
 /// write functions to assign values to the check box fields and dynamically grey them out when
 // one is selected.
 
-const form_fields = {
-  startDate: undefined,
-  endDate: undefined,
-  firstName: undefined,
-  lastName: undefined,
-  phoneNumber: undefined,
-  email: undefined,
-  address: undefined,
-  employeeStatus: undefined,
-  typeOfEmployee: undefined,
-  fromPublicService: undefined,
-  SupervisorOrManager: undefined,
-  performancePay: undefined,
-  buildingLocation: undefined,
-  teleworkOrOnsite: undefined,
-  employeeTitle: undefined,
-  employeeSector: undefined,
-  employeeBranch: undefined,
-  employeeDirectorate: undefined,
-  SupervisorName: undefined,
-  numberOfMonitors: undefined,
-  KeyboardAndMouse: undefined,
-  otherEquipment: undefined,
-  cellphone: undefined,
-  cellSupervisorOrManager: undefined,
-  mailboxAccess: undefined,
-  software: undefined,
-  Gcdocs: undefined,
-  other: undefined,
-};
-=======
->>>>>>> parent of 799f872 (created form object)
+// const form_fields = {
+//   startDate: undefined,
+//   endDate: undefined,
+//   firstName: undefined,
+//   lastName: undefined,
+//   phoneNumber: undefined,
+//   email: undefined,
+//   address: undefined,
+//   employeeStatus: undefined,
+//   typeOfEmployee: undefined,
+//   fromPublicService: undefined,
+//   SupervisorOrManager: undefined,
+//   performancePay: undefined,
+//   buildingLocation: undefined,
+//   teleworkOrOnsite: undefined,
+//   employeeTitle: undefined,
+//   employeeSector: undefined,
+//   employeeBranch: undefined,
+//   employeeDirectorate: undefined,
+//   SupervisorName: undefined,
+//   numberOfMonitors: undefined,
+//   KeyboardAndMouse: undefined,
+//   otherEquipment: undefined,
+//   cellphone: undefined,
+//   cellSupervisorOrManager: undefined,
+//   mailboxAccess: undefined,
+//   software: undefined,
+//   Gcdocs: undefined,
+//   other: undefined,
+// };
 
 function coreEm() {
   if (coreEmployeeElement === null) {
@@ -50,8 +60,3 @@ function coreEm() {
     console.log(coreEmployeeElement.value);
   }
 }
-<<<<<<< HEAD
-=======
-
-coreEmployeeElement.addEventListener('click', coreEm);
->>>>>>> parent of 799f872 (created form object)
