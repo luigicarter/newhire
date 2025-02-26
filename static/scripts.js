52806f176652e1c3385b6f3cb49697f1be5ae724
luigicarter <propersuccess12@gmail.com>
2025-02-25 22:46:52 -0500

    added to do tasks in scripts file

--- static/scripts.js
+++ static/scripts.js
@@ -1,6 +1,10 @@
 const coreEmployeeElement = document.getElementById('core-personel');
 const employee = document.getElementById('employ-status');
 const start_date = document.getElementById('start-date');
+/// add all fields except for check box fields and dropdown
+
+/// write functions to assign values to the check box fields and dynamically grey them out when 
+// one is selected. 
 
 const form_fields = {
   startDate: undefined,
@@ -41,18 +45,3 @@ function coreEm() {
   }
 }
 
-// function dropdown_selection(component) {
-//   if (component === ){
-
-//   }
-// }
-
-start_date.addEventListener("", () => {
-  if (start_date)
-});
-
-employee.addEventListener('click', (e) => {
-  console.log(e.value);
-});
-
-console.log();
