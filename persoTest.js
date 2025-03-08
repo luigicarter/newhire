e40a57204094de084ecf7655495220c0c3873ae3
luigiCarter <propersuccess12@gmail.com>
2025-03-08 02:17:59 -0500

    added fucntion t0 get fomr object from json file

--- persoTest.js
+++ persoTest.js
@@ -1,10 +1,24 @@
-
-/// this file is to test different methods and strategies
-var firsName = 'ahmed';
-var lastName = 'hersi';
+import { PDFDocument, StandardFonts, charSplit, rgb } from 'pdf-lib'
+import fs from "fs"
 
 
-import {testExport}  from "./jsFiles/pdfCreation.js"
+
+let pathToJsonFile = "form_json_files/newHireJson.json"
+function GetObjectFromJson(hash){
+    let file;
+    file = fs.readFileSync(pathToJsonFile, (error, data) => {
+        file = data
+        
+    })
+
+    file = JSON.parse(file)
+    
+    file = file[hash]
+    return file
+    
+        
+}
+
+let form = getFormDAta("2b71b79479ad0046ad0659df52dca2f271cc30cfea293d6fab4e5d2da5472e43")
 
 
-testExport()
